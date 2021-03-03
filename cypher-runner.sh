2b74b4a6fb4921ef10248efd8b8ad15b3c228bf7
Alan Christie <alan.christie@matildapeak.com>
2021-03-03 16:44:42 +0000

    - Propagated key elements of cypher-runner from 3.5.25 branch
    - Password setting now responsibility of docker-entrypoint
    - Fixed enterprise gds registration
    - Updated libs

--- cypher-runner.sh
+++ cypher-runner.sh
@@ -7,9 +7,11 @@
 #
 # Expects the following environment variables: -
 #
-#   CYPHER_PRE_NEO4J_SLEEP
+#   ACTION_SLEEP_TIME           (default of 4 seconds)
 #   CYPHER_ROOT
 #   GRAPH_PASSWORD
+#   NEO4J_dbms_directories_data (default '/data')
+#   NEO4J_dbms_directories_logs (default '/logs')
 
 ME=cypher-runner.sh
 
@@ -26,29 +28,54 @@ CYPHER_PATH="$CYPHER_ROOT/cypher-script"
 ONCE_SCRIPT="$CYPHER_PATH/cypher-script.once"
 ALWAYS_SCRIPT="$CYPHER_PATH/cypher-script.always"
 
-# Files created (touched) when the 'first' script is run
-# and when the 'always' script is run. These files are created
+# Files created (touched) when the 'once' script is run and completed
+# and when the 'always' script is run and complete. These files are created
 # even if there are no associated scripts. The 'always' file
-# is erased each time we're executed and re-created after it's re-executed.
+# is erased each time the container starts and re-created after the script has
+# finished.
 ONCE_EXECUTED_FILE="$CYPHER_PATH/once.executed"
+ONCE_STARTED_FILE="$CYPHER_PATH/once.started"
 ALWAYS_EXECUTED_FILE="$CYPHER_PATH/always.executed"
+ALWAYS_STARTED_FILE="$CYPHER_PATH/always.started"
 
 # Always remove the ALWAYS_EXECUTED_FILE.
 # We re-create this when we've run the always script
 # (which happens every time we start)
 rm -f "$ALWAYS_EXECUTED_FILE" || true
 
-echo "($ME) $(date) GRAPH_PASSWORD=$GRAPH_PASSWORD"
-echo "($ME) $(date) ONCE_SCRIPT=$ONCE_SCRIPT"
-echo "($ME) $(date) ALWAYS_SCRIPT=$ALWAYS_SCRIPT"
-echo "($ME) $(date) ONCE_EXECUTED_FILE=$ONCE_EXECUTED_FILE"
-echo "($ME) $(date) ALWAYS_EXECUTED_FILE=$ALWAYS_EXECUTED_FILE"
+ACTION_SLEEP_TIME=${ACTION_SLEEP_TIME:-4}
+NEO4J_dbms_directories_data=${NEO4J_dbms_directories_data:-/data}
+NEO4J_dbms_directories_logs=${NEO4J_dbms_directories_logs:-/logs}
 
-# Configurable sleep prior to the first cypher command.
-# Needs to be sufficient to allow the server to start accepting connections.
-SLEEP_TIME=${CYPHER_PRE_NEO4J_SLEEP:-60}
-echo "($ME) $(date) Pre-cypher pause ($SLEEP_TIME seconds)..."
-sleep "$SLEEP_TIME"
+echo "($ME) $(date) ALWAYS_EXECUTED_FILE=$ALWAYS_EXECUTED_FILE"
+echo "($ME) $(date) ALWAYS_SCRIPT=$ALWAYS_SCRIPT"
+echo "($ME) $(date) ACTION_SLEEP_TIME=$ACTION_SLEEP_TIME"
+echo "($ME) $(date) CYPHER_ROOT=$CYPHER_ROOT"
+echo "($ME) $(date) GRAPH_PASSWORD=$GRAPH_PASSWORD"
+echo "($ME) $(date) NEO4J_dbms_directories_data=$NEO4J_dbms_directories_data"
+echo "($ME) $(date) NEO4J_dbms_directories_logs=$NEO4J_dbms_directories_logs"
+echo "($ME) $(date) ONCE_SCRIPT=$ONCE_SCRIPT"
+echo "($ME) $(date) ONCE_EXECUTED_FILE=$ONCE_EXECUTED_FILE"
+
+# The graph service has not started if there's no debug file.
+DEBUG_FILE="$NEO4J_dbms_directories_logs/debug.log"
+echo "($ME) $(date) Checking $DEBUG_FILE..."
+until [ -f "$DEBUG_FILE" ]; do
+  echo "($ME) $(date) Waiting for $DEBUG_FILE..."
+  sleep "$ACTION_SLEEP_TIME"
+done
+
+# Wait until a 'ready' line exists in the debug log...
+echo "($ME) $(date) Checking ready line in $DEBUG_FILE..."
+READY=$(grep -c "Database.*graph[.]db.* is ready." < "$DEBUG_FILE")
+until [ "$READY" -eq "1" ]; do
+  echo "($ME) $(date) Waiting for ready line in $DEBUG_FILE..."
+  sleep "$ACTION_SLEEP_TIME"
+  READY=$(grep -c "Database.*graph[.]db.* is ready." < "$DEBUG_FILE")
+done
+
+echo "($ME) $(date) Post ready pause..."
+sleep "$ACTION_SLEEP_TIME"
 
 # Run the ONCE_SCRIPT
 # (if the ONCE_EXECUTED_FILE is not present)...
@@ -57,10 +84,11 @@ if [[ ! -f "$ONCE_EXECUTED_FILE" && -f "$ONCE_SCRIPT" ]]; then
     echo "[SCRIPT BEGIN]"
     cat "$ONCE_SCRIPT"
     echo "[SCRIPT END]"
+    touch "$ONCE_STARTED_FILE"
     until /var/lib/neo4j/bin/cypher-shell -u neo4j -p "$GRAPH_PASSWORD" < "$ONCE_SCRIPT"
     do
-        echo "($ME) $(date) No joy, waiting..."
-        sleep 4
+        echo "($ME) $(date) No joy with .once, waiting and trying again..."
+        sleep "$ACTION_SLEEP_TIME"
     done
     echo "($ME) $(date) .once script executed."
 else
@@ -75,10 +103,11 @@ if [ -f "$ALWAYS_SCRIPT" ]; then
     echo "[SCRIPT BEGIN]"
     cat "$ALWAYS_SCRIPT"
     echo "[SCRIPT END]"
+    touch "$ALWAYS_STARTED_FILE"
     until /var/lib/neo4j/bin/cypher-shell -u neo4j -p "$GRAPH_PASSWORD" < "$ALWAYS_SCRIPT"
     do
-        echo "($ME) $(date) No joy, waiting..."
-        sleep 4
+        echo "($ME) $(date) No joy with .always, waiting and trying again..."
+        sleep "$ACTION_SLEEP_TIME"
     done
     echo "($ME) $(date) .always script executed."
 else
